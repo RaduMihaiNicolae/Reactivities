a2d72f44c4ac68cc11294b7107dc5fb118a6a93a
NicolaeRDU <mihairadu8039@gmail.com>
2026-03-20 12:48:37 +0200

    End of section 6

--- client/src/features/activities/dashboard/ActivityCard.tsx
+++ client/src/features/activities/dashboard/ActivityCard.tsx
@@ -1,12 +1,14 @@
 import { Box, Button, Card, CardActions, CardContent, Chip, Typography } from "@mui/material"
+import { useActivities } from "../../../lib/hooks/useActivities"
 
 type Props = {
     activity: Activity
     selectActivity: (id: string) => void
-    deleteActivity: (id: string) => void
 }
 
-export default function ActivityCard({ activity, selectActivity, deleteActivity }: Props) {
+export default function ActivityCard({ activity, selectActivity }: Props) {
+    const { deleteActivity } = useActivities();
+
     return (
         <Card sx={{ borderRadius: 3 }}>
             <CardContent>
@@ -19,7 +21,14 @@ export default function ActivityCard({ activity, selectActivity, deleteActivity 
                 <Chip label={activity.category} variant="outlined" />
                 <Box display='flex' gap={3}>
                     <Button size="medium" variant="contained" onClick={() => selectActivity(activity.id)}>View</Button>
-                    <Button color="error" size="medium" variant="contained" onClick={() => deleteActivity(activity.id)}>Delete</Button>
+                    <Button
+                        disabled={deleteActivity.isPending}
+                        color="error"
+                        size="medium"
+                        variant="contained"
+                        onClick={() => deleteActivity.mutate(activity.id)}
+                    >
+                        Delete</Button>
                 </Box>
             </CardActions>
         </Card >
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
-import mkcert from 'vite-plugin-mkcert'
+// import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +11,6 @@ export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
-    mkcert()
+    // mkcert()
   ],
 })
